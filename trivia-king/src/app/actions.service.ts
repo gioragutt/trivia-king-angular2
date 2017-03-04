81edbd59eeb5dd3d3490609172e69d6475cd2df4
Giora Guttsait <giora111@gmail.com>
2017-03-04 20:53:13 +0200

    Trivia King - updated Actions service to have methods to dispatch actions on questions

--- trivia-king/src/app/actions.service.ts
+++ trivia-king/src/app/actions.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
+import { NgRedux } from '@angular-redux/store';
 import { Action } from 'redux';
+import { IAppState } from './reducers';
+import { TriviaQuestion, TriviaQuestionState } from './model';
 
 @Injectable()
 export class Actions {
@@ -7,5 +10,26 @@ export class Actions {
   static QUESTION_SHOW_QUESTION = 'QUESTION_SHOW_QUESTION';
   static QUESTION_SHOW_ANSWER = 'QUESTION_SHOW_ANSWER';
 
-  hideQuestion()
+  constructor(private ngRedux: NgRedux<IAppState>) { }
+
+  dispatchQuestionUpdate(question: TriviaQuestion, action: string) {
+    this.ngRedux.dispatch({
+      type: action,
+      payload: {
+        question
+      }
+    });
+  }
+
+  hideQuestion(question: TriviaQuestion) {
+    this.dispatchQuestionUpdate(question, Actions.QUESTION_HIDE);
+  }
+
+  showQuestion(question: TriviaQuestion) {
+    this.dispatchQuestionUpdate(question, Actions.QUESTION_SHOW_QUESTION);
+  }
+
+  showAnswer(question: TriviaQuestion) {
+    this.dispatchQuestionUpdate(question, Actions.QUESTION_SHOW_ANSWER);
+  }
 }
