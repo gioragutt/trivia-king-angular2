b2ebd963830232b47e8ad2bb4f206053882639b6
Giora Guttsait <giora111@gmail.com>
2017-03-04 20:55:16 +0200

    Trivia King - updated components to use flexbox instead of a gridlist
    * this ends up giving the wanted result of a fixed sized grid
    * questions now dispatch updates
    * todo: make cells larger, more dense, color coded by state of question

--- trivia-king/src/app/question/question.component.ts
+++ trivia-king/src/app/question/question.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TriviaQuestion } from '../model';
+import { Actions } from '../actions.service';
 
 @Component({
   selector: 'trk-question',
@@ -8,4 +9,18 @@ import { TriviaQuestion } from '../model';
 })
 export class QuestionComponent {
   @Input() question: TriviaQuestion;
+
+  constructor(private actions: Actions) { }
+
+  changeToHidden() {
+    this.actions.hideQuestion(this.question);
+  }
+
+  changeToQuestion() {
+    this.actions.showQuestion(this.question);
+  }
+
+  changeToAnswer() {
+    this.actions.showAnswer(this.question);
+  }
 }
