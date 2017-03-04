4761c4bd2680c8a0818610366d112cc7cb913b7b
Giora Guttsait <giora111@gmail.com>
2017-03-05 01:32:56 +0200

    Trivia King - added credits to menu

--- trivia-king/src/app/app.component.ts
+++ trivia-king/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { select } from '@angular-redux/store';
 import { Observable } from 'rxjs/Observable';
 import { TriviaQuestions } from './model';
+import { MdSnackBar } from '@angular/material';
 
 @Component({
   selector: 'trk-root',
@@ -9,6 +10,14 @@ import { TriviaQuestions } from './model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    @select() categories$: Observable<string[]>;
-    @select() questions$: Observable<TriviaQuestions>;
+  @select() categories$: Observable<string[]>;
+  @select() questions$: Observable<TriviaQuestions>;
+
+  constructor(public snackBar: MdSnackBar) { }
+
+  openCredit() {
+    this.snackBar.open('נכתב על ידי: גיורא גוטצייט', 'סגור', {
+      duration: 3500,
+    });
+  }
 }
