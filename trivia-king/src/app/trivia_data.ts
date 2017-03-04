8a828bf71ba65505cb9fe3ac3157cbb13026736a
Giora Guttsait <giora111@gmail.com>
2017-03-04 20:16:16 +0200

    Trivia King - changed mock data to be generated randomly
    * to ease development, I changed the mock data to be generated randomly
    * each question and answer is a string between 10 to 50 characters long

--- trivia-king/src/app/trivia_data.ts
+++ trivia-king/src/app/trivia_data.ts
@@ -1,16 +1,36 @@
 import { TriviaQuestions } from './model';
 
-export const MockTriviaCategories: string[] = [
-    'עובדות חשובות',
-    'עוד משהו'
-];
-
-export const MockTriviaData: TriviaQuestions = [
-    {
-        question: 'מי זה',
-        answer: 'מה זה',
-        category: 'עובדות חשובות',
-        score: 100,
-        state: 'hidden'
+export const MockTriviaCategories: string[] = function() {
+    const categories: string[] = [];
+    for (let i = 1; i <= 10; ++i) {
+        categories.push('category' + i);
     }
-];
+    return categories;
+}();
+
+const stringOfRandomSizeBetween = (min: number, max: number): string => {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < Math.random() * 100; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+};
+
+export const MockTriviaData: TriviaQuestions = function() {
+    const questions: TriviaQuestions = [];
+    MockTriviaCategories.forEach((category: string, index: number) => {
+        for (let i = 1; i <= 5; ++i) {
+            questions.push({
+                score: 100 * i,
+                answer: 'A: ' + stringOfRandomSizeBetween(10, 50),
+                question: 'Q: ' + stringOfRandomSizeBetween(10, 50),
+                category,
+                state: 'show_answer'
+            });
+        }
+    });
+    return questions;
+}();
