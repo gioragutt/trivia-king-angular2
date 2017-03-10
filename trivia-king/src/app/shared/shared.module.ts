afe2b552ba9480cf4b761875a4278e37bd0cd179
Giora Guttsait <giora111@gmail.com>
2017-03-10 19:47:28 +0200

    Trivia King - created a shared module
    * this module will have components, pipes, and etc that are to be used by other modules

---
+++ trivia-king/src/app/shared/shared.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '@angular/material';
+
+@NgModule({
+  imports: [CommonModule, MaterialModule],
+})
+export class SharedModule { }
