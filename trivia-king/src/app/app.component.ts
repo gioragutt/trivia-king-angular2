1330761a4855a229e61a9452319092f54de78032
Giora Guttsait <giora111@gmail.com>
2017-03-10 18:17:21 +0200

    Trivia King - app component now saves the store state to the local storage
    * this fixes the problem that if someone refreshes the page, the app state will be retained (as long as cache is not removed)

--- trivia-king/src/app/app.component.ts
+++ trivia-king/src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from './local-storage.service';
+import { NgRedux } from '@angular-redux/store';
+import { IAppState } from './reducers';
 
 @Component({
   selector: 'trk-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(private localStorage: LocalStorageService, private store: NgRedux<IAppState>) {
+    store.subscribe(() => {
+      console.log('Saving state to local storage');
+      localStorage.saveToLocalStorage('state', store.getState());
+    });
+  }
+}
