f9bedadfe2322801ff3bdca8966735716992b872
Giora Guttsait <giora111@gmail.com>
2017-03-11 17:18:52 +0200

    Trivia King - added renaming and deleting of teams

--- trivia-king/src/app/actions/team.actions.ts
+++ trivia-king/src/app/actions/team.actions.ts
@@ -2,24 +2,36 @@ import { Injectable } from '@angular/core';
 import { NgRedux } from '@angular-redux/store';
 import { Action } from 'redux';
 import { IAppState } from '../reducers';
-import { TriviaQuestion, TriviaQuestionState } from '../model';
+import { TriviaTeam } from '../model';
 
 @Injectable()
 export class TeamActions {
   static TEAM_ADD = 'TEAM_ADD';
-  static TEAM_CHANGE_NAME = 'TEAM_CHANGE_NAME';
+  static TEAM_RENAME = 'TEAM_RENAME';
   static TEAM_ASSIGN_CORRECT_ANSWER = 'TEAM_ASSIGN_CORRECT_ANSWER';
   static TEAM_REMOVE_CORRECT_ANSWER = 'TEAM_REMOVE_CORRECT_ANSWER';
   static TEAM_REMOVE = 'TEAM_REMOVE';
 
   constructor(private ngRedux: NgRedux<IAppState>) { }
 
+  removeTeam(team: TriviaTeam) {
+    this.ngRedux.dispatch({
+      type: TeamActions.TEAM_REMOVE,
+      payload: { team }
+    });
+  }
+
+  renameTeam(team: TriviaTeam, newName: string) {
+    this.ngRedux.dispatch({
+      type: TeamActions.TEAM_RENAME,
+      payload: { team, newName }
+    });
+  }
+
   addTeam(name: string) {
     this.ngRedux.dispatch({
       type: TeamActions.TEAM_ADD,
-      payload: {
-        name
-      }
+      payload: { name }
     });
   }
 }
--- trivia-king/src/app/reducers/team.ts
+++ trivia-king/src/app/reducers/team.ts
@@ -2,20 +2,49 @@ import { Action, updateObject } from './index';
 import { TriviaTeam, TriviaTeams } from '../model';
 import { TeamActions } from '../actions';
 
+const isTeamInState = (teams: TriviaTeams, predicate: (team: TriviaTeam) => boolean) => {
+    return teams.find(predicate) !== undefined;
+}
+
 const addTeam = (state: TriviaTeams, name: string): TriviaTeams => {
     if (name === undefined || name.length === 0) {
+        console.log('Trying to add team with invalid name:', name);
         return state;
     }
-    if (state.find((team: TriviaTeam) => team.name === name) !== undefined) {
+    if (isTeamInState(state, team => team.name === name)) {
+        console.log('Team', name, 'already in teams list');
         return state;
     }
     return [...state, { name, correctAnswers: [] }];
+};
+
+const removeTeam = (state: TriviaTeams, team: TriviaTeam): TriviaTeams => {
+    if (!isTeamInState(state, teamInState => teamInState === team)) {
+        console.log('Trying to remove team', team, 'which is not in the teams list');
+    }
+    return state.filter(teamInState => teamInState !== team);
+};
+
+const renameTeam = (state: TriviaTeams, team: TriviaTeam, newName: string): TriviaTeams => {
+    if (!isTeamInState(state, teamInState => teamInState === team)) {
+        console.log('Trying to remove team', team, 'which is not in the teams list');
+    }
+    return state.map((teamInState: TriviaTeam, index: number): TriviaTeam => {
+        if (teamInState === team) {
+            return updateObject(team, { name: newName });
+        }
+        return teamInState;
+    });
 };
 
 export const teams = (state: TriviaTeams = [], action: Action): TriviaTeams => {
     switch (action.type) {
         case TeamActions.TEAM_ADD:
             return addTeam(state, action.payload.name);
+        case TeamActions.TEAM_REMOVE:
+            return removeTeam(state, action.payload.team);
+        case TeamActions.TEAM_RENAME:
+            return renameTeam(state, action.payload.team, action.payload.newName);
     }
     return state;
 };
