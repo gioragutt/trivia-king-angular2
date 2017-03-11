b814b4024c8dff147dfa64cbb63d0f37ba1e8257
Giora Guttsait <giora111@gmail.com>
2017-03-11 17:20:08 +0200

    Trivia King - added renaming and removing of teams from the setup page

--- trivia-king/src/app/pages/setup-page/team-info-preview/team-info-preview.component.ts
+++ trivia-king/src/app/pages/setup-page/team-info-preview/team-info-preview.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TriviaTeam } from '../../../model';
+import { TeamActions } from '../../../actions/team.actions';
 
 @Component({
   selector: 'trk-team-info-preview',
@@ -8,4 +9,35 @@ import { TriviaTeam } from '../../../model';
 })
 export class TeamInfoPreviewComponent {
   @Input() team: TriviaTeam;
+  nameToEdit: string;
+  isEditing = false;
+
+  constructor(private actions: TeamActions) { }
+
+  disableEdit() {
+    this.isEditing = false;
+  }
+
+  enableEdit() {
+    this.isEditing = true;
+  }
+
+  onStartRename() {
+    this.enableEdit();
+    this.nameToEdit = this.team.name;
+  }
+
+  onTeamRename() {
+    this.disableEdit();
+    this.actions.renameTeam(this.team, this.nameToEdit);
+  }
+
+  cancelRename() {
+    this.disableEdit();
+  }
+
+  removeTeam() {
+    this.disableEdit();
+    this.actions.removeTeam(this.team);
+  }
 }
