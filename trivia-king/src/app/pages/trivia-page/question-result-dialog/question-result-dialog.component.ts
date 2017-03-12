211d8c45d7adb18e47c7befb16da42bf1a0ae5f8
Giora Guttsait <giora111@gmail.com>
2017-03-12 22:27:26 +0200

    Trivia King - question result dialog now uses the new queries service
    * removed usage of select() in the component
    * I still had to provide the teams list somehow, so I've used the fact that the service's select isn't private

--- trivia-king/src/app/pages/trivia-page/question-result-dialog/question-result-dialog.component.ts
+++ trivia-king/src/app/pages/trivia-page/question-result-dialog/question-result-dialog.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { MdDialogRef } from '@angular/material';
-import { TriviaQuestion, TriviaTeams, TriviaTeam } from '../../../model';
-import { select } from '@angular-redux/store';
-import { Observable } from 'rxjs/Observable';
+import { TriviaQuestion, TriviaTeam } from '../../../model';
+import { StoreQueriesService } from '../../../shared';
 
 export interface TrkQuestionResultDialogConfig {
   question: TriviaQuestion;
@@ -14,22 +13,17 @@ export interface TrkQuestionResultDialogConfig {
   styleUrls: ['./question-result-dialog.component.css']
 })
 export class QuestionResultDialogComponent {
-  @select() teams$: Observable<TriviaTeams>;
   question: TriviaQuestion;
   selectedTeam: string;
 
-  constructor(public dialogRef: MdDialogRef<QuestionResultDialogComponent>) {
-    this.teams$.subscribe(teams => {
+  constructor(public dialogRef: MdDialogRef<QuestionResultDialogComponent>, private query: StoreQueriesService) {
       const config = <TrkQuestionResultDialogConfig>dialogRef.config.data;
       if (config === undefined) {
         console.log('Received undefined configuration for the modal');
       }
       this.question = config.question;
-      const team = teams.find(t => t.correctAnswers.find(q => q.id === this.question.id) !== undefined);
-      if (team !== undefined) {
-        this.selectedTeam = team.name;
-      }
-    });
+      this.query.getAssignedTeamForQuestion(this.question)
+        .filter((t: TriviaTeam) => t !== undefined)
+        .subscribe((t: TriviaTeam) => this.selectedTeam = t.name);
    }
-
 }
